0fd1dce953e3df8c6a2765994cc052a31a2c5b50
btousifar <bn.tousifar86@gmail.com>
2024-10-02 21:07:21 -0500

    finish the UI part of the project
    revision the code in main branch and add div
    element as the container and also three buttons
    live inside another div. use querySelector to get
    the hold of the buttons and added eventListener
    with arrow function to set up the callback
    function (playRound).
    
    [Ticket: X]

--- logic.js
+++ logic.js
@@ -1,7 +1,3 @@
-// console.log("Hello")
-
-let humanScore = 0;
-let computerScore = 0;
 
 function getComputerChoice() {
     // GENERATE RANDOM NUMBER BETWEEN 1 TO 3
@@ -20,92 +16,107 @@ function getComputerChoice() {
 }
 
 
-function getHumanChoice() {
-    human_choice = prompt("what is your choice? Rock Paper or Scissors? ").toLowerCase()
-    // console.log(human_choice)
+// function getHumanChoice() {
+//     human_choice = prompt("what is your choice? Rock Paper or Scissors? ").toLowerCase()
+//     // console.log(human_choice)
 
-    while (human_choice !== "rock" && human_choice !== "paper" && human_choice !== "scissors") {
-        alert ("invalid input")
-        human_choice = prompt("what is your choice? Rock Paper or Scissors? ").toLowerCase()
-    }
+//     while (human_choice !== "rock" && human_choice !== "paper" && human_choice !== "scissors") {
+//         alert ("invalid input")
+//         human_choice = prompt("what is your choice? Rock Paper or Scissors? ").toLowerCase()
+//     }
    
-    return human_choice
+//     return human_choice
     
-}
-
-let computer_choice;
-let your_choice;
-let is_game_on = true
+// }
 
 
-
-function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice) {
+    computerChoice = getComputerChoice();
+    const result = document.querySelector(".result");
+    
     if (humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "paper" && computerChoice === "rock" || humanChoice === "scissors" && computerChoice === "paper") {
         console.log(`Yay! you beat it this round!`)
+        result.textContent = `Player: ${humanChoice} +++ Computer: ${computerChoice}. Yay! you beat it this round!`;
+        result.style.backgroundColor = 'aquamarine'
         humanScore +=1;
         
         
     }
     else if (humanChoice === computerChoice) {
-        console.log(`you both have ${humanChoice}. This round is tied.`)
+        console.log(`Player and Computer have ${humanChoice}. This round is tied.`)
+        result.textContent = `Player and Computer have ${humanChoice}. This round is tied.`;
+        result.style.backgroundColor = 'rgb(252, 252, 99)'
     }
     else {
         console.log(`You lose this round!`)
+        result.textContent = `Player: ${humanChoice} +++ Computer: ${computerChoice}.You lose this round!`;
+        result.style.backgroundColor = 'lightcoral'
         computerScore +=1;
     }
+
+    div.append(result)
+    scoreUpdate(humanScore, computerScore);
+    
 }
 
-function playGame(rounds) {
-    // if (humanScore == 3) {
-    //     console.log(`You Win! your score is ${humanScore} , computer score is ${computerScore}`);
-    //     is_game_on = false;
-    // }
-    // else if (computerScore == 3) {
-    //     console.log(`You lose! your score is ${humanScore} , computer score is ${computerScore}`);
-    //     is_game_on = false;
-    // }
-    // else {
-        
-    //     computer_choice = getComputerChoice();
-    //     your_choice = getHumanChoice()
-    //     console.log(`YOU: ${your_choice}`);
-    //     console.log(`COMPUTER: ${computer_choice}`);
-        
-    //     playRound(your_choice, computer_choice);
+function scoreUpdate(humanScore, computerScore) {
+    
+    const scoreboard = document.querySelector(".scoreboard");
+    scoreboard.textContent = `your score: ${humanScore} ****** computer score: ${computerScore}`;
+    div.append(scoreboard)
 
-    //     // Pause before clearing to allow the user to see the results of the round.
-    //     alert("Press OK to continue to the next round");
-    //     console.clear();
-    // }
-
-    for (i = 0; i < rounds; i++) {
-        computer_choice = getComputerChoice();
-        your_choice = getHumanChoice()
-        console.log(`YOU: ${your_choice}`);
-        console.log(`COMPUTER: ${computer_choice}`);
-        
-        playRound(your_choice, computer_choice);
-        console.log(`your score: ${humanScore}`)
-        console.log(`computer score: ${computerScore}`)
-
-        // Pause before clearing to allow the user to see the results of the round.
-        alert("Press OK to continue to the next round");
-        console.clear();
-    }
-
-    if (humanScore > computerScore) {
-        console.log(`You Win! your score is ${humanScore} , computer score is ${computerScore}`);
-    }
-    else if (computerScore > humanScore) {
-        console.log(`You lose! your score is ${humanScore} , computer score is ${computerScore}`);
-    }
-    else {
-        console.log(`the game is draw. both got the score of ${humanScore}`);
-    }
-}  
+}
 
 
-playGame(5);
+let computer_choice;
+// let your_choice;
+let humanScore = 0;
+let computerScore = 0;
+
+
+// your_choice = getHumanChoice()
+// console.log(`YOU: ${your_choice}`);
+// console.log(`COMPUTER: ${computer_choice}`);
+
+const div = document.querySelector("div");
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+
+rock.addEventListener("click", () => {
+    playRound(rock.id);
+}); 
+paper.addEventListener("click", () => {
+    playRound(paper.id);
+});
+scissors.addEventListener("click", () => {
+    playRound(scissors.id);
+});
+// console.log(rock.id);
+
+// playRound(your_choice, computer_choice);
+// console.log(`your score: ${humanScore}`)
+// console.log(`computer score: ${computerScore}`)
+
+// // Pause before clearing to allow the user to see the results of the round.
+// alert("Press OK to continue to the next round");
+// console.clear();
+
+
+// if (humanScore > computerScore) {
+//     console.log(`You Win! your score is ${humanScore} , computer score is ${computerScore}`);
+// }
+// else if (computerScore > humanScore) {
+//     console.log(`You lose! your score is ${humanScore} , computer score is ${computerScore}`);
+// }
+// else {
+//     console.log(`the game is draw. both got the score of ${humanScore}`);
+// }
+  
+
+
+
     
     
     
